f72c938f4ebfc9977fcc45036875be85f6f26c03
ducdk <ducvuk6196@gmail.com>
2024-02-06 10:23:38 +0700

    fix

--- js/animation.js
+++ js/animation.js
@@ -37,4 +37,41 @@ $(window).on('load', function () {
       x: "400",
     })
   }
+  if ($(window).width() <= 768) {
+    gsap.registerPlugin("ScrollTrigger");
+    gsap.to("#nguoi", {
+      scrollTrigger:{
+        trigger: "#gsap1",
+        scrub: 1,
+        markers: false
+      },
+      y: -280,
+    })
+    
+    gsap.to("#background", {
+      scrollTrigger:{
+        trigger: "#gsap1",
+        scrub: 1,
+        markers: false
+      },
+      y: -260,
+    })
+    gsap.to("#nen", {
+      scrollTrigger:{
+        trigger: "#gsap1",
+        scrub: 1,
+        markers: false
+      },
+      y: -260,
+    })
+    
+    gsap.to("#texttet", {
+      scrollTrigger:{
+        trigger: "#gsap1",
+        scrub: 1,
+        markers: false
+      },
+      y: 100,
+    })
+  }
 });
